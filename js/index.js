7a7b3ac3be7dbb24e2225c81bc4b68aa5730f735
Hill Crest Way <peixoto.ale@outlook.com>
2024-07-27 00:51:07 +0000

    mobile resp changes

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+  const navToggle = document.getElementById('nav-toggle');
+  const navMenu = document.getElementById('nav-menu');
+
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+
